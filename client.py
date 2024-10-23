13c80c379ac11f76d04bcbd72b8da49c056b69c3
Brunu <comachiobelobruno@protonmail.com>
2024-10-23 18:38:57 -0400

    Added: user prompt for currency and value

--- client.py
+++ client.py
@@ -3,15 +3,16 @@ import currency_converter_pb2
 import currency_converter_pb2_grpc
 
 def run():
+    # Solicita as informações do usuário
+    from_currency = input("Digite a moeda de origem (ex: USD): ").upper()
+    to_currency = input("Digite a moeda de destino (ex: BRL): ").upper()
+    amount = float(input("Digite o valor a ser convertido: "))
+
     # Conectar ao servidor
-    with grpc.insecure_channel('192.168.207.206:50051') as channel:
+    with grpc.insecure_channel('localhost:50051') as channel:  # Substituir pelo IP do servidor
         stub = currency_converter_pb2_grpc.CurrencyConverterStub(channel)
         
         # Solicitar a conversão
-        from_currency = 'EUR'
-        to_currency = 'BRL'
-        amount = 100.0
-        
         request = currency_converter_pb2.ConvertRequest(from_currency=from_currency, to_currency=to_currency, amount=amount)
         try:
             response = stub.Convert(request)
